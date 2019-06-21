b7e7bbfaeac2f53450191d9df81fd5d1d53ced37
Airthee <13355624+Airthee@users.noreply.github.com>
2019-06-21 11:23:54 +0200

    Improve performances

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,9 @@ static void read_csv(const string& filename, vector<Mat>& images, vector<int>& l
         CV_Error(CV_StsBadArg, error_message);
     }
     string line, path, classlabel;
+    stringstream liness;
     while (getline(file, line)) {
-        stringstream liness(line);
+        liness = stringstream(line);
         getline(liness, path, separator);
         getline(liness, classlabel);
         if(!path.empty() && !classlabel.empty()) {
@@ -48,6 +49,7 @@ int main(int argc, const char *argv[]) {
     // These vectors hold the images and corresponding labels:
     vector<Mat> images;
     vector<int> labels;
+
     // Read in the data (fails if no valid input filename is given, but you'll get an error message):
     try {
         read_csv(fn_csv, images, labels);
@@ -56,19 +58,21 @@ int main(int argc, const char *argv[]) {
         // nothing more we can do
         exit(1);
     }
+
     // Get the height from the first image. We'll need this
     // later in code to reshape the images to their original
     // size AND we need to reshape incoming faces to this size:
     int im_width = images[0].cols;
     int im_height = images[0].rows;
+
     // Create a FaceRecognizer and train it on the given images:
     Ptr<FaceRecognizer> model = createFisherFaceRecognizer();
     model->train(images, labels);
+    
     // That's it for learning the Face Recognition model. You now
     // need to create the classifier for the task of Face Detection.
     // We are going to use the haar cascade you have specified in the
     // command line arguments:
-    //
     CascadeClassifier haar_cascade;
     haar_cascade.load(fn_haar);
     // Get a handle to the Video device:
@@ -81,28 +85,31 @@ int main(int argc, const char *argv[]) {
 
     // Store prediction
     int lastPrediction = -1;
+    int prediction = -1;
 
     // Holds the current frame from the Video device:
-    Mat frame;
-    for(;;) {
+    Mat frame, original, gray, face_resized;
+    vector< Rect_<int> > faces;
+    Rect face_i;
+    Mat face;
+    while(true) {
         cap >> frame;
         // Clone the current frame:
-        Mat original = frame.clone();
+        original = frame.clone();
         // Convert the current frame to grayscale:
-        Mat gray;
         cvtColor(original, gray, CV_BGR2GRAY);
         // Find the faces in the frame:
-        vector< Rect_<int> > faces;
+        faces = vector< Rect_<int>>();
         haar_cascade.detectMultiScale(gray, faces);
         // At this point you have the position of the faces in
         // faces. Now we'll get the faces, make a prediction and
         // annotate it in the video. Cool or what?
         for(int i = 0; i < faces.size(); i++) {
             // Process face by face:
-            Rect face_i = faces[i];
+            face_i = faces[i];
 
             // Crop the face from the image. So simple with OpenCV C++:
-            Mat face = gray(face_i);
+            face = gray(face_i);
 
             // Resizing the face is necessary for Eigenfaces and Fisherfaces. You can easily
             // verify this, by reading through the face recognition tutorial coming with OpenCV.
@@ -114,21 +121,19 @@ int main(int argc, const char *argv[]) {
             //
             // Since I am showing the Fisherfaces algorithm here, I also show how to resize the
             // face you have just found:
-            Mat face_resized;
             cv::resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
 
             // Now perform the prediction, see how easy that is:
-            int prediction = model->predict(face_resized);
+            prediction = model->predict(face_resized);
 
             // And finally write all we've found out to the original image!
             // First of all draw a green rectangle around the detected face:
-            rectangle(original, face_i, CV_RGB(0, 255,0), 1);
+            // rectangle(original, face_i, CV_RGB(0, 255,0), 1);
 
             // Create the text we will annotate the box with:
-            string box_text = format("Prediction = %d", prediction);
             if (prediction != lastPrediction) {
                 lastPrediction = prediction;
-                cout << box_text << endl;
+                cout << format("Prediction = %d", prediction) << endl;
             }
 
             // Calculate the position for annotated text (make sure we don't
@@ -137,14 +142,14 @@ int main(int argc, const char *argv[]) {
             int pos_y = std::max(face_i.tl().y - 10, 0);
 
             // And now put it into the image:
-            putText(original, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0,255,0), 2.0);
+            // putText(original, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0,255,0), 2.0);
         }
         // Show the result:
         // imshow("face_recognizer", original);
         // And display it:
-        char key = (char) waitKey(20);
+
         // Exit this loop on escape:
-        if(key == (int)'q')
+        if((char)waitKey(20) == (int)'q')
             break;
     }
     return 0;
